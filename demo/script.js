306d7cdcdd53d2f0ecf96a55471392a72352ac07
zengdamo <zengdamo@163.com>
2016-08-31 11:05:48 +0800

    改动2

--- demo/script.js
+++ demo/script.js
@@ -1,2 +1,10 @@
 // demo javascript
 console.log('hello 世界.');
+
+function myfunction(){
+    var x = '', i =0;
+    for (i = 0; i<5; i++){
+        x = x + 'the number is' + i + '<br>';
+    }
+    document.getElementById('demo').innerHTML = x;
+}
